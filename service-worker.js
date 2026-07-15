c4b5fefd0139c3aac3abfe41463490675df43da2
shiho-3 <shihof3@gmail.com>
2026-07-15 13:12:49 +0900

    2.4

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='project-nanpre-v2-3';
+const CACHE='project-nanpre-v2-4';
 const ASSETS=['./','index.html','manifest.webmanifest','icons/icon-192.png','icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
